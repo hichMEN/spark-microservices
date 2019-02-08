c395cdbe78b32eb335f763c35e91864154580b2b
Hichem CHEBIHI <hichem-externe.chebihi@edf.fr>
2019-02-08 14:26:21 +0100

    first commit

---
+++ src/main/java/org/openmind/demos/sparkmicroservices/SparkMicroservicesApplication.java
@@ -0,0 +1,14 @@
+package org.openmind.demos.sparkmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SparkMicroservicesApplication.class, args);
+    }
+
+}
+
